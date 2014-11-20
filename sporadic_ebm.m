e29eb9da79becc223a180f13d7c24eeab73dc2c6
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2014-11-20 16:39:20 +0000

    finished implementing the MCMC sampling and greedy ascent .. now testing everything since I don't get the same answer as Alex's paper

--- sporadic_ebm.m
+++ sporadic_ebm.m
@@ -6,10 +6,14 @@ function [] = sporadic_ebm()
 % EBMevents - labels of the EBM events
 load('alex_data/ADNIdata_Baseline.mat')
 
-[mu_mix, sigma_mix, pi_mix] = calc_gaussian_parameters(EBMdataBL, EBMdxBL)
-load('gaussian_params.mat')
+%[mu_mix, sigma_mix, pi_mix] = calc_gaussian_parameters(EBMdataBL, EBMdxBL)
+load('sporadic_params.mat')
 
-char_seq_grad_asc(EBMdataBL, mu_mix, sigma_mix)
+%grad_asc_S = char_seq_grad_asc(EBMdataBL, mu_mix, sigma_mix)
+%save('sporadic_params.mat', 'grad_asc_S','-append')
+MCMC_samples = MCMC_sampling(EBMdataBL, mu_mix, sigma_mix, grad_asc_S);
+
+calc_variance_diagrams(samples);
 
 %calc_likelihood(EBMdataBL, 1:14, mu_mix, sigma_mix)
 
@@ -83,7 +87,7 @@ for biomk=1:nr_biomarkers
    
 end
 
-   save('gaussian_params.mat', 'mu_mix', 'sigma_mix', 'pi_mix'); 
+   save('sporadic_params.mat', 'mu_mix', 'sigma_mix', 'pi_mix'); 
    
 
 end
@@ -197,7 +201,7 @@ function logL = calc_likelihood(X, S, mu_mix, sigma_mix)
 % modelled from equation (1), alex paper page 2567
 % J - # patients
 % I - # Events or disease stages
-[J,I] = size(X) 
+[J,I] = size(X);
 
 
 [I2,~] = size(mu_mix);
@@ -211,6 +215,9 @@ logL = 1;
 mu_mix = mu_mix(S,:);
 sigma_mix = sigma_mix(S,:);
 
+%also reorder the dataset
+X = X(:,S);
+
 % x_{ij} biomarker i in subject j
 % pXgE(i,j,1) = p (x_{ij} | E_i)      patients
 % pXgE(i,j,2) = p (x_{ij} | not E_i)  controls
@@ -219,26 +226,29 @@ pXgE = zeros(I, J, 2);
 % for each biomarker 
 % mu_mix(:,1) - controls
 % mu_mix(:,2) - patients
-for i=1:I
-    pXgE(i,:,1) = normpdf(X(:,i), mu_mix(i,2), sigma_mix(i,2));
-    pXgE(i,:,2) = normpdf(X(:,i), mu_mix(i,1), sigma_mix(i,1));
+for biomk=1:I
+    pXgE(biomk,:,1) = normpdf(X(:,biomk), mu_mix(biomk,2), sigma_mix(biomk,2));
+    pXgE(biomk,:,2) = normpdf(X(:,biomk), mu_mix(biomk,1), sigma_mix(biomk,1));
+    if(~all(pXgE(biomk,:,1)))
+        display('pXgE is zero')
+    end
 end
 
 pK = 1/J; % uniform prior that patient i is at stage k.
 
 % for each patient
-for j=1:J
+for patient=1:J
    sum = 0;
    % for each disease stage
-   for k=0:I
+   for stage=0:I
         
-        sum = sum + prod(pXgE(1:k,j,1)) * prod(pXgE(k+1:I,j,2));
+        sum = sum + prod(pXgE(1:stage,patient,1)) * prod(pXgE(stage+1:I,patient,2));
    end
-   if(sum * pK == 0)
+   if(sum == 0)
       display('Warning log-likelihood is -inf') 
    end
    
-   logL = logL  + log(sum * pK)
+   logL = logL  + log(sum * pK);
    
 end
 
@@ -260,7 +270,11 @@ curr_seq = 1:14;
 nr_iterations = 2000;
 old_likelihood = calc_likelihood(X, curr_seq, mu_mix, sigma_mix);
 
+s = RandStream('mcg16807','Seed',0);
+RandStream.setGlobalStream(s);
+
 for i=1:nr_iterations
+    i
     p1 = ceil(rand * 14);
     p2 = ceil(rand * 14);
     while (p1 == p2)
@@ -283,6 +297,82 @@ end
 
 end
 
+% generic function for
+function samples = MCMC_sampling(X, mu_mix, sigma_mix, init_seq)
+
+[nr_subjects,nr_biomk] = size(X);
+
+
+[nr_biomk2,~] = size(mu_mix);
+[nr_biomk3,~] = size(sigma_mix);
+
+assert(nr_biomk == nr_biomk2 && nr_biomk == nr_biomk3);
+
+% initialise curr_seq to initial sequence
+curr_seq = init_seq;
+
+%burnout_iterations = 10^5;
+burnout_iterations = 100;
+actual_iterations = 10^6;
+
+old_likelihood = calc_likelihood(X, curr_seq, mu_mix, sigma_mix);
+
+s = RandStream('mcg16807','Seed',0);
+RandStream.setGlobalStream(s);
+
+samples = zeros(actual_iterations, nr_biomk);
+
+for i=1:(burnout_iterations + actual_iterations)
+    i
+    p1 = ceil(rand * 14);
+    p2 = ceil(rand * 14);
+    while (p1 == p2)
+        p2 = ceil(rand * 14);
+    end
+    new_seq = curr_seq;
+    tmp = new_seq(p1);
+    new_seq(p1) = new_seq(p2);
+    new_seq(p2) = tmp;
+    new_likelihood = calc_likelihood(X, new_seq, mu_mix, sigma_mix);
+    likely_ratio = new_likelihood / old_likelihood;
+    
+   
+    if(rand < likely_ratio)
+        % swapt them with probability min(likely_ratio, 1)
+        curr_seq = new_seq;
+        old_likelihood = new_likelihood;
+    end
+    
+    if (i > burnout_iterations)
+        samples(i-burnout_iterations,:) = curr_seq;
+    end
+    
+    if(mod(i,1000) == 0)
+       display('1000 iterations') 
+    end
+    
+end
+
+
+end
+
+% function l = get_likelyhood_ratio(new_likelihood, old_likelihood, mode)
+% 
+% switch mode
+%     
+%     case 'MCMC'
+%         l = new_likelihood / old_likelihood;
+%     case 'greedy'
+%         if (new_likelihood > old_likelihood)
+%             l = 1;
+%         else
+%             l = 0;
+%         end
+%     otherwise
+%         error('mode has to be MCMC or greedy')
+% end
+%     
+% end
 
 function Y = eval_mix_gaussians(X, mu, sigma, pi)
 % X is a vector of data points
@@ -298,4 +388,14 @@ for k=1:K
    end
 end
 
+end
+
+function [] = calc_variance_diagrams(samples)
+    
+biomk_avg_ordering = mean(MCMC_samples);
+[~, char_sequence] = sort(biomk_avg_ordering);
+char_sequence
+
+
+    
 end
